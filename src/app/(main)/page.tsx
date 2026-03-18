328d1e79e91210d40d255f331d65ab8b1a794db4
Brubru <jordan.sankare@gmail.com>
2026-03-18 17:55:06 +0100

    fix pt

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -3,7 +3,7 @@ import Header from "@/components/home/Header";
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center font-sans">
-      <main className="flex min-h-screen w-full flex-col items-center justify-between pt-20 sm:items-start">
+      <main className="flex min-h-screen w-full flex-col items-center justify-between sm:items-start">
         <Header title="Une petite faim ?" subtitle="En quelques clics, trouvez des repas proches de chez vous" />
       </main>
     </div>
